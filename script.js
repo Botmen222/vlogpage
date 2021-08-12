f6bb77d03da47d59f8a3940ad75f5fe8ee911648
botmen222 <zomboribotond22@gmail.com>
2021-08-12 15:58:42 +0200

    js

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log("hello")
+const menu =document.querySelector(".dots");
+side_bar = document.querySelector("aside");
+let menuState = false;
+let x = window.matchMedia("(max-width: 700px)");
+menu.addEventListener('click', showhideMenu);
+function showhideMenu(){
+    if(!menuState){
+        menu.classList.add("active");
+        side_bar.classList.add("active");
+        menuState = true;
+    }else{
+        menu.classList.remove("active");
+        side_bar.classList.remove("active");
+        menuState = false;
+    }
+}
+function myfunction(x) {
+    if (x.matches) {
+        menu.classList.add("active");
+        side_bar.classList.add("active");
+    } else {
+        menu.classList.remove("active");
+        side_bar.classList.remove("active");
+    }
+}
+myfunction(x);
+x.addListener(myfunction);
